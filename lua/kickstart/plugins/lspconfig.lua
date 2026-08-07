5645b63b3be9c35530d686461f0b2d6afe2b6ced
Malte Frank Gerdes <malte.f.gerdes@gmail.com>
2026-08-07 10:12:50 +0200

    lspconfig: Use rust_analyzer.

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -118,7 +118,7 @@ local servers = {
   },
   gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
+  rust_analyzer = {},
   --
   -- Some languages (like typescript) have entire language plugins that can be useful:
   --    https://github.com/pmizio/typescript-tools.nvim
