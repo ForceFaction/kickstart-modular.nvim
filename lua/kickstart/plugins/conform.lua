1b69158838219ea05c5831316742ee395cfc291a
Malte Frank Gerdes <malte.f.gerdes@gmail.com>
2026-08-07 10:14:05 +0200

    conform: Use typstyle as typsts formatter.

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -27,6 +27,12 @@ require('conform').setup {
     --
     -- You can use 'stop_after_first' to run the first available formatter from the list
     -- javascript = { "prettierd", "prettier", stop_after_first = true },
+    typst = { 'typstyle' },
+  },
+  formatters = {
+    typstyle = {
+      prepend_args = { '--wrap-text' },
+    },
   },
 }
 
